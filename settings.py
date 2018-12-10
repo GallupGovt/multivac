df11bbc1239e4d29aadea46b32600b92d08edb4b
Peter Rasmussen <Peter_Rasmussen@gallup.com>
2018-12-10 16:36:49 -0600

    Wrote script to retrieve raw data

--- settings.py
+++ settings.py
@@ -3,13 +3,31 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+# define search terms
+terms = ['sir model', 'susceptible-infected-recovered', 'irSIR model']
 
 # define data directories
 data_dir = Path('.') /'data'
+
+# raw directories
 raw_dir = data_dir / 'raw'
+arxiv_dir = raw_dir / 'arxiv' 
+springer_dir = raw_dir / 'springer'
+pubmed_dir = raw_dir / 'pubmed'
+
+# interim / in-between directory
 interim_dir = data_dir / 'interim'
+
+# processed / finished data ready to be input into model
 processed_dir = data_dir / 'processed'
+metadata_dir = processed_dir / 'metadata'
+
+# models directory
+models_dir = Path('.') / 'models'
+
+dirs = [data_dir, raw_dir, arxiv_dir, springer_dir, pubmed_dir, interim_dir
+       ,processed_dir, metadata_dir, models_dir]
 
 # make data directories if they don't already exist
-for _dir in [raw_dir, interim_dir, processed_dir]:
+for _dir in dirs:
     utilities.mkdir(_dir)
