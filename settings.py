2ea294082d114f253d8a0a4748d70269d04695f6
Peter Rasmussen <Peter_Rasmussen@gallup.com>
2018-12-11 16:16:11 -0600

    Finish first cut of data acquisition / scraping

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-from src import utilities
+from multivac.src import utilities
 from pathlib import Path
 from dotenv import load_dotenv
 import os
@@ -8,14 +8,16 @@ terms = ['sir model', 'susceptible-infected-recovered', 'irSIR model'
         ,'susceptible-infected', 'seir model'
         ,'susceptible-exposed-infected-recovered']
 
+# specify sources
+sources = ['arxiv', 'pubmed', 'springer']
+
+root_dir = Path('.') / 'multivac'
+
 # define data directories
-data_dir = Path('.') /'data'
+data_dir = root_dir / 'data'
 
 # raw directories
 raw_dir = data_dir / 'raw'
-arxiv_dir = raw_dir / 'arxiv' 
-springer_dir = raw_dir / 'springer'
-pubmed_dir = raw_dir / 'pubmed'
 
 # interim / in-between directory
 interim_dir = data_dir / 'interim'
@@ -25,11 +27,10 @@ processed_dir = data_dir / 'processed'
 metadata_dir = processed_dir / 'metadata'
 
 # models directory
-models_dir = Path('.') / 'models'
-
-dirs = [data_dir, raw_dir, arxiv_dir, springer_dir, pubmed_dir, interim_dir
-       ,processed_dir, metadata_dir, models_dir]
+models_dir = root_dir / 'models'
 
 # make data directories if they don't already exist
+dirs = [data_dir, raw_dir, interim_dir, processed_dir, metadata_dir, models_dir]
+dirs += [raw_dir / x for x in sources]
 for _dir in dirs:
     utilities.mkdir(_dir)
