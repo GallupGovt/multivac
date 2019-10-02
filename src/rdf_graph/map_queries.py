8ddfc5f6d0a40b07f6299f0bab916005ad95e82c
Ben Ryan <ben_ryan@WDCRYANBMACX10B.T-mobile.com>
2019-10-02 14:21:04 -0400

    Updated query mapping functionality.

--- src/rdf_graph/map_queries.py
+++ src/rdf_graph/map_queries.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import os
+import pandas as pd
 import re
 import tensorflow as tf
 
@@ -23,30 +24,33 @@ def predict(con, h, r, t, num_top_rel=10, threshold=.1):
         result = []
 
         for head in heads:
-            result.append((head, r, t), con.predict_triple(head, t, r, threshold))
+            result.append(((head, r, t), con.predict_triple(head, t, r, threshold)))
 
     elif r == -1:
         rels = con.predict_relation(h, t, num_top_rel)
         result = []
 
         for rel in rels:
-            result.append((h, rel, t), con.predict_triple(h, rel, r, threshold))
+            result.append(((h, rel, t), con.predict_triple(h, t, rel, threshold)))
     elif t == -1:
         tails = con.predict_tail_entity(h, r, num_top_rel)
         result = []
 
         for tail in tails:
-            result.append((h, r, tail), con.predict_triple(h, tail, r))
+            result.append(((h, r, tail), con.predict_triple(h, tail, r)))
     else:
         result = [(h,r,t), con.predict_triple(h, t, r, threshold)]
 
-    return acc
+    return result
 
 def cos_sim(x, test):
-    x = x.split(" | ")
-    return max([cosine.normalized_similarity(test, item) for item in x])
+    if pd.notnull(x):
+        x = x.split(" | ")
+        return max([cosine.normalized_similarity(test, item) for item in x])
+    else:
+        return x
 
-def get_answers(con, query, direc, model, num_top_rel=10, threshold=.1):
+def get_answers(con, query, num_top_rel=10, threshold=.1):
     """
         This function takes a query to the knowledge graph and outputs
         predicted relationships that have the highest accuracy. 
@@ -62,36 +66,21 @@ def get_answers(con, query, direc, model, num_top_rel=10, threshold=.1):
         relationships in the network graph.
     """
 
-    con = config.Config()
-    con.set_in_path(direc)
-    con.set_work_threads(8)
-    con.set_dimension(100)
-    con.set_test_link_prediction(True)
-    con.set_test_triple_classification(True)
+    if 'subject' not in query:
+        query['subject'] = ""
+    if 'object' not in query:
+        query['object'] = ""
+    if 'relation' not in query:
+        query['relation'] = ""
+
+    direc = con.get_in_path()
 
     top_subj = np.ones(num_top_rel)*-1
     top_rel  = np.ones(num_top_rel)*-1
     top_obj  = np.ones(num_top_rel)*-1
 
-    files = glob.glob('{}*tf*'.format(direc))
-
-    if not files:
-        raise Exception('No models to predict on; generate one first.')
-    else:
-        times = list(set([file.split('.')[2] for file in files]))
-        ifile = max([datetime.strptime(x, '%d%b%Y-%H:%M:%S') for
-                    x in times]).strftime('%d%b%Y-%H:%M:%S')
-        con.set_import_files('{}model.vec.{}.tf'.format(direc, ifile))
-
-    # initialize settings
-    con.init()
-
-    # set knowledge embedding model
-    kem = set_model_choice(args_dict['model'])
-    con.set_model(kem)
-
     # identify files for use
-    files = glob.glob('{}/*.txt'.format(direc))
+    files = glob.glob(os.path.join(direc,'*.txt'))
     rel_file = max([file for file in files if re.search('relation', file)],
                    key=os.path.getctime)
     ent_file = max([file for file in files if re.search('entity', file)],
@@ -107,7 +96,9 @@ def get_answers(con, query, direc, model, num_top_rel=10, threshold=.1):
             subj_id = -1
         else:
             # Potentially use np.argmax() for multiple max results
-            subj_id = entities.Ent.iloc[subj_scores.idxmax()]
+            subj_id = entities.Id.iloc[subj_scores.idxmax()]
+    else:
+        subj_id = -1
 
     if len(query['object']) > 0:
         obj_scores = entities.Ent.apply(lambda x: cos_sim(x, query['object']))
@@ -116,16 +107,20 @@ def get_answers(con, query, direc, model, num_top_rel=10, threshold=.1):
             obj_id = -1
         else:
             # Potentially use np.argmax() for multiple max results
-            obj_id = entities.Ent.iloc[obj_scores.idxmax()]
+            obj_id = entities.Id.iloc[obj_scores.idxmax()]
+    else:
+        obj_id = -1
 
     if len(query['relation']) > 0:
-        rel_scores = entities.Rel.apply(lambda x: cos_sim(x, query['relation']))
+        rel_scores = relations.Rel.apply(lambda x: cos_sim(x, query['relation']))
 
         if rel_scores.max() < threshold:
             rel_id = -1
         else:
             # Potentially use np.argmax() for multiple max results
-            rel_id = entities.Rel.iloc[rel_scores.idxmax()]
+            rel_id = relations.Id.iloc[rel_scores.idxmax()]
+    else:
+        rel_id = -1
 
     #
     # YOU'VE GOT YOUR TOP MATCHES - WHAT DO YOU DO NOW??
@@ -133,7 +128,12 @@ def get_answers(con, query, direc, model, num_top_rel=10, threshold=.1):
 
     # Apply this to each cell:
 
-    result = predict(con, subj_id, rel_id, obj_id, num_top_rel, threshold)
+    try:
+        result = predict(con, subj_id, rel_id, obj_id, num_top_rel, threshold)
+    except:
+        print(query)
+        print((subj_id, rel_id, obj_id))
+        raise Exception
 
     return result
 
@@ -161,7 +161,7 @@ def predicted_object(con, query, num_top_rel=10, max_digits_ent=1000,
         relation = 'relation2id-{}.txt'.format(args_dict['filetime'])
         entity = 'entity2id-{}.txt'.format(args_dict['filetime'])
     else:
-        files = glob.glob('{}/*.txt'.format(args_dict['dir']))
+        files = glob.glob(os.path.join(args_dict['dir'],'*.txt'))
         relation = max([file for file in files if re.search('relation', file)],
                        key=os.path.getctime)
         entity = max([file for file in files if re.search('entity', file)],
@@ -223,22 +223,32 @@ def predicted_object(con, query, num_top_rel=10, max_digits_ent=1000,
 
     # develop network data
     nets = [x for x in net_rel if x[2] > threshold]
-    with open('{}network_{}_{}.json'
-              .format(args_dict['out'], args_dict['search'].replace(' ', '-'),
-                      args_dict['timestamp']), 'w') as f:
+    with open(os.path.join(args_dict['out'],'network_{}_{}.json'
+              .format(args_dict['search'].replace(' ', '-'),
+                      args_dict['timestamp'])), 'w') as f:
         json.dump(nets, f)
 
 
     print(out)
-    with open('{}prediction_{}_{}.json'
-              .format(args_dict['out'], args_dict['search'].replace(' ', '-'),
-                      args_dict['timestamp']), 'w') as f:
+    with open(os.path.join(args_dict['out'],'prediction_{}_{}.json'
+              .format(args_dict['search'].replace(' ', '-'),
+                      args_dict['timestamp'])), 'w') as f:
         json.dump(out, f)
 
 
 def run(args_dict):
     # setup
     timestamp = datetime.now().strftime('%d%b%Y-%H:%M:%S')
+
+    if args_dict['threshold'] is None:
+        threshold = 0.1
+    else:
+        threshold = float(args_dict['threshold'])
+
+    if args_dict['num_top_rel'] is None:
+        num_top_rel = 10
+    else:
+        num_top_rel = args_dict['num_top_rel']
 
     # check if output directory exists
     if not os.path.isdir(args_dict['out']):
@@ -248,7 +258,10 @@ def run(args_dict):
     con = config.Config()
 
     # set global parameters
-    con.set_in_path(args_dict['dir'])
+    if args_dict['dir'].endswith(os.path.sep):
+        con.set_in_path(args_dict['dir'])
+    else:
+        con.set_in_path(args_dict['dir']+os.path.sep)
     con.set_work_threads(8)
     con.set_dimension(100)
 
@@ -262,25 +275,26 @@ def run(args_dict):
         con.set_ent_neg_rate(1)
         con.set_rel_neg_rate(0)
         con.set_opt_method("SGD")
-        con.set_export_files('{}model.vec.{}.tf'.format(args_dict['out'],
-                                                         timestamp), 0)
+        con.set_export_files(os.path.join(args_dict['out'],
+                                          'model.vec.{}.tf'.format(timestamp)), 
+                             0)
 
         # save out model parameters
-        con.set_out_files('{}embedding.vec.{}.json'.format(args_dict['out'],
-                                                            timestamp))
+        con.set_out_files(os.path.join(args_dict['out'],
+                                       'embedding.vec.{}.json'.format(timestamp)))
     else:
         con.set_test_link_prediction(True)
         con.set_test_triple_classification(True)
 
-        files = glob.glob('{}*tf*'.format(args_dict['out']))
+        files = glob.glob(os.path.join(args_dict['out'],'*tf*'))
         if not files:
             raise Exception('No models to predict on; generate one first.')
         else:
             times = list(set([file.split('.')[2] for file in files]))
             ifile = max([datetime.strptime(x, '%d%b%Y-%H:%M:%S') for
                         x in times]).strftime('%d%b%Y-%H:%M:%S')
-            con.set_import_files('{}model.vec.{}.tf'.format(args_dict['out'],
-                                                            ifile))
+            con.set_import_files(os.path.join(args_dict['out'],
+                                              'model.vec.{}.tf'.format(ifile)))
             args_dict.update({'timestamp': ifile})
 
     # initialize settings
@@ -299,28 +313,49 @@ def run(args_dict):
         if not args_dict['search']:
             raise Exception('You need to provide a search term.')
         else:
-            predicted_object(con, args_dict['search'])
+            if os.path.exists(args_dict['search']):
+                queries = pd.read_csv(args_dict['search'])
 
-            # create network graph
-            with open('{}network_{}_{}.json'
-                      .format(args_dict['out'],
-                              args_dict['search'].replace(' ', '-'),
-                              args_dict['timestamp']), 'r') as f:
-                net_data = json.load(f)
+                query = lambda z: {x:y 
+                                   for x, y 
+                                   in zip(['subject','relation','object'],
+                                          z.split()[:3])}
 
-            el = {}
-            [el[str(item[0])].append(item[1]) if str(item[0]) in
-             el.keys() else el.update({str(item[0]): [item[1]]}) for
-             item in net_data]
+                triples = queries.Annotations.apply(lambda x: query(x))
+                results = triples.apply(lambda x: get_answers(con, x, 
+                                                              num_top_rel, 
+                                                              threshold))
+                # results = triples.apply(lambda x: predict(con, 
+                #                                           *x, 
+                #                                           num_top_rel, 
+                #                                           threshold))
+                results.to_csv(os.path.join(args_dict['dir'],"results.csv"))
+            else:
+                predicted_object(con, 
+                                 args_dict['search'], 
+                                 num_top_rel, 
+                                 threshold)
 
-            net = nx.Graph(el)
-            nx.draw(net, pos=nx.spring_layout(net), alpha=.75, node_size=100)
-            plt.savefig('{}network_{}_{}.png'
-                        .format(args_dict['out'],
-                                args_dict['search'].replace(' ', '-'),
-                                args_dict['timestamp']),
-                        pad_inches=.05, orientation='landscape',
-                        papertype='letter')
+
+
+            # # create network graph
+            # with open(os.path.join(args_dict['out'],'network_{}_{}.json'
+            #           .format(args_dict['search'].replace(' ', '-'),
+            #                   args_dict['timestamp'])), 'r') as f:
+            #     net_data = json.load(f)
+
+            # el = {}
+            # [el[str(item[0])].append(item[1]) if str(item[0]) in
+            #  el.keys() else el.update({str(item[0]): [item[1]]}) for
+            #  item in net_data]
+
+            # net = nx.Graph(el)
+            # nx.draw(net, pos=nx.spring_layout(net), alpha=.75, node_size=100)
+            # plt.savefig(os.path.join(args_dict['out'],'network_{}_{}.png'
+            #             .format(args_dict['search'].replace(' ', '-'),
+            #                     args_dict['timestamp'])),
+            #             pad_inches=.05, orientation='landscape',
+            #             papertype='letter')
 
 
 def set_model_choice(model):
@@ -362,8 +397,14 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--run', required=True, choices=['fit', 'model'],
                         help='Identify choice of action, fitting a model or '
                         'predicting from it.')
-    parser.add_argument('-s', '--search', required=False, help='Search phrase '
-                        'for execution.')
+    parser.add_argument('-t', '--threshold', required=False, 
+                        help='Threshold accuracy for matches; default is 0.1.')
+    parser.add_argument('-n', '--num_top_rel', required=False, 
+                        help='Number of top matches to return; default is 10.')
+    parser.add_argument('-s', '--search', required=False, 
+                        help='Searches to execute. Either a path to a CSV '
+                        'containing triples or a triple in the format '
+                        '"subject terms ::: relation terms ::: object terms"')
     args_dict = vars(parser.parse_args())
 
     run(args_dict)
