780e888b59151224d6a057cefac24b24f5855ac8
Ben Ryan <ben_ryan@WDCRYANBMACX10B.T-mobile.com>
2019-10-17 16:47:39 -0400

    Discriminator component model adapted for use on MULTIVAC. Now accepts query data divided into "good" and "bad" queries, and trains on identifying which is which.

--- src/gan/discriminator/treelstm/model.py
+++ src/gan/discriminator/treelstm/model.py
@@ -85,3 +85,66 @@ class SimilarityTreeLSTM(nn.Module):
         rstate, rhidden = self.childsumtreelstm(rtree, rinputs)
         output = self.similarity(lstate, rstate)
         return output
+
+
+# # module for QueryGAN discriminator, adapting ChildSumTreeLSTM
+# class ChildSumTreeLSTM(nn.Module):
+#     def __init__(self, in_dim, mem_dim):
+#         super(ChildSumTreeLSTM, self).__init__()
+#         self.in_dim = in_dim
+#         self.mem_dim = mem_dim
+#         self.ioux = nn.Linear(self.in_dim, 3 * self.mem_dim)
+#         self.iouh = nn.Linear(self.mem_dim, 3 * self.mem_dim)
+#         self.fx = nn.Linear(self.in_dim, self.mem_dim)
+#         self.fh = nn.Linear(self.mem_dim, self.mem_dim)
+#         self.final = nn.Linear(self.mem_dim, self.mem_dim)
+
+#     def node_forward(self, inputs, child_c, child_h):
+#         child_h_sum = torch.sum(child_h, dim=0, keepdim=True)
+
+#         iou = self.ioux(inputs) + self.iouh(child_h_sum)
+#         i, o, u = torch.split(iou, iou.size(1) // 3, dim=1)
+#         i, o, u = F.sigmoid(i), F.sigmoid(o), F.tanh(u)
+
+#         f = F.sigmoid(
+#             self.fh(child_h) +
+#             self.fx(inputs).repeat(len(child_h), 1)
+#         )
+#         fc = torch.mul(f, child_c)
+
+#         c = torch.mul(i, u) + torch.sum(fc, dim=0, keepdim=True)
+#         h = torch.mul(o, F.tanh(c))
+#         return c, h
+
+#     def forward(self, tree, inputs):
+#         for idx in range(tree.num_children):
+#             self.forward(tree.children[idx], inputs)
+
+#         if tree.num_children == 0:
+#             child_c = inputs[0].detach().new(1, self.mem_dim).fill_(0.).requires_grad_()
+#             child_h = inputs[0].detach().new(1, self.mem_dim).fill_(0.).requires_grad_()
+#         else:
+#             child_c, child_h = zip(* map(lambda x: x.state, tree.children))
+#             child_c, child_h = torch.cat(child_c, dim=0), torch.cat(child_h, dim=0)
+
+#         c, h = self.node_forward(inputs[tree.idx], child_c, child_h)
+#         result = F.sigmoid(self.final(h), 1)
+        
+#         return result
+
+class QueryGAN_Discriminator(nn.Module):
+    def __init__(self, vocab_size, in_dim, mem_dim, sparsity, freeze):
+        super().__init__()
+        self.emb = nn.Embedding(vocab_size, in_dim, padding_idx=Constants.PAD, sparse=sparsity)
+        if freeze:
+            self.emb.weight.requires_grad = False
+        self.childsumtreelstm = ChildSumTreeLSTM(in_dim, mem_dim)
+        self.confidence = nn.Linear(mem_dim, 1)
+
+    def forward(self, tree, inputs):
+        inputs = self.emb(inputs)
+        state, hidden = self.childsumtreelstm(tree, inputs)
+        output = F.sigmoid(self.confidence(state))
+        return output
+
+
--- src/gan/discriminator/treelstm/trainer.py
+++ src/gan/discriminator/treelstm/trainer.py
@@ -20,37 +20,43 @@ class Trainer(object):
         self.model.train()
         self.optimizer.zero_grad()
         total_loss = 0.0
-        indices = torch.randperm(len(dataset), dtype=torch.long, device='cpu')
+        indices = torch.randperm(len(dataset), dtype=torch.long, device=self.device)
+
         for idx in tqdm(range(len(dataset)), desc='Training epoch ' + str(self.epoch + 1) + ''):
-            ltree, linput, rtree, rinput, label = dataset[indices[idx]]
-            target = utils.map_label_to_target(label, dataset.num_classes)
-            linput, rinput = linput.to(self.device), rinput.to(self.device)
-            target = target.to(self.device)
-            output = self.model(ltree, linput, rtree, rinput)
-            loss = self.criterion(output, target)
+            tree, inputs, label = dataset[indices[idx]]
+            inputs = inputs.to(self.device)
+            label = label.to(self.device)
+            output = self.model(tree, inputs)
+            try:
+                loss = self.criterion(output, label)
+            except:
+                import pdb; pdb.set_trace()
             total_loss += loss.item()
             loss.backward()
+
             if idx % self.args.batchsize == 0 and idx > 0:
                 self.optimizer.step()
                 self.optimizer.zero_grad()
+
         self.epoch += 1
         return total_loss / len(dataset)
 
     # helper function for testing
     def test(self, dataset):
         self.model.eval()
+
         with torch.no_grad():
             total_loss = 0.0
-            predictions = torch.zeros(len(dataset), dtype=torch.float, device='cpu')
-            indices = torch.arange(1, dataset.num_classes + 1, dtype=torch.float, device='cpu')
+            predictions = torch.zeros(len(dataset), dtype=torch.float, device=self.device)
+            indices = torch.arange(0, 2, dtype=torch.float, device=self.device)
+
             for idx in tqdm(range(len(dataset)), desc='Testing epoch  ' + str(self.epoch) + ''):
-                ltree, linput, rtree, rinput, label = dataset[idx]
-                target = utils.map_label_to_target(label, dataset.num_classes)
-                linput, rinput = linput.to(self.device), rinput.to(self.device)
-                target = target.to(self.device)
-                output = self.model(ltree, linput, rtree, rinput)
-                loss = self.criterion(output, target)
+                tree, inputs, label = dataset[idx]
+                inputs, label = inputs.to(self.device), label.to(self.device)
+                output = self.model(tree, inputs)
+                loss = self.criterion(output, label)
                 total_loss += loss.item()
                 output = output.squeeze().to('cpu')
-                predictions[idx] = torch.dot(indices, torch.exp(output))
+                predictions[idx] = torch.round(output)
+
         return total_loss / len(dataset), predictions
