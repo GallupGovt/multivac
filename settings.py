b9063786fe7f8ae5db651cf1c0eff309ce38fa17
Mitchell Bregman <mitchbregs@gmail.com>
2019-10-21 12:53:07 -0400

    restoring from previous PR, docker-compose for all services, flask app gui, reconfig

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-from multivac.src import utilities
+from src import utilities
 
 
 cfg = configparser.ConfigParser()
@@ -18,12 +18,13 @@ except NameError:
 root_dir = cfg['PATHS'].get('root_dir', cfgDIR)
 qgnet_dir = cfg['PATHS'].get('qgnet_dir', root_dir / '..' / 'qgnet')
 
-data_dir = cfg['PATHS'].get('data_dir', root_dir/'data')
+sys_dir = cfg['PATHS'].get('sys_dir', root_dir/'sys')
+data_dir = cfg['PATHS'].get('data_dir', sys_dir/'data')
 raw_dir = cfg['PATHS'].get('raw_dir', data_dir/'raw')
 interim_dir = cfg['PATHS'].get('interim_dir', data_dir/'interim')
 processed_dir = cfg['PATHS'].get('processed_dir', data_dir/'processed')
 metadata_dir = cfg['PATHS'].get('metadata_dir', processed_dir/'metadata')
-models_dir = cfg['PATHS'].get('models_dir', root_dir/'models')
+models_dir = cfg['PATHS'].get('models_dir', sys_dir/'models')
 stanf_nlp_dir = cfg['PATHS'].get('stanf_nlp_dir',
                                  root_dir/'stanford_nlp_models')
 mln_dir = cfg['PATHS'].get('mln_dir', root_dir/'mln_models')
