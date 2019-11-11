93738db8321854c6c1f0f970b42a33d31a5540de
Ben Ryan <ben_ryan@wdcryanbmacx10b.gallup.com>
2019-11-11 13:58:15 -0500

    Fixing discriminator imports.

--- src/gan/discriminator/treelstm/__init__.py
+++ src/gan/discriminator/treelstm/__init__.py
@@ -1,12 +1,12 @@
 from . import Constants
-from .dataset import SICKDataset, MULTIVACDataset
+from .dataset import MULTIVACDataset
 from .metrics import Metrics
-from .model import SimilarityTreeLSTM, QueryGAN_Discriminator
+from .model import QueryGAN_Discriminator
 from .trainer import Trainer
 from .tree import Tree
 from . import utils
 from .vocab import Vocab
 
-__all__ = [Constants, SICKDataset, MULTIVACDataset, Metrics, 
-           SimilarityTreeLSTM, QueryGAN_Discriminator, Trainer, 
+__all__ = [Constants, MULTIVACDataset, Metrics, 
+           QueryGAN_Discriminator, Trainer, 
            Tree, Vocab, utils]
