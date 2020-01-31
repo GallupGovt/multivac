Integrating policy gradient fixes and CNN discriminator change together.

src/gan/discriminator/model.py
@@ -88,8 +88,6 @@ class QueryGAN_Discriminator_CNN(nn.Module):
             yhat = self(X).softmax(dim=-1)
 
             scores, labels = yhat.topk(1, -1, True, True)
-            labels.apply_(lambda x: self.args['labels'][x])
-
             return scores, labels
 
     def train_single_code(self, train):
@@ -113,6 +111,9 @@ class QueryGAN_Discriminator_CNN(nn.Module):
                                                  amsgrad=True)
         #epochs_no_improve = 0
         #max_test_acc = 0
+        if self.args['device'] == 'cuda':
+            self.cuda()
+            self.optimizer.cuda()
 
         self.train()
 

src/gan/gen_pyt/model/parser.py
@@ -195,6 +195,7 @@ class Parser(nn.Module):
         if args['cuda']:
             self.new_long_tensor = torch.cuda.LongTensor
             self.new_tensor = torch.cuda.FloatTensor
+            self.to('cuda')
         else:
             self.new_long_tensor = torch.LongTensor
             self.new_tensor = torch.FloatTensor
@@ -922,19 +923,26 @@ class Parser(nn.Module):
         """
 
         if hyp is not None and hyp.completed:
-            pass
+            result = hyp
         else:
+            beam_size = 1
+
             while True:
                 result = self.parse(src_sent=src_sent, 
                                   hyp=hyp, 
                                   states=states, 
                                   return_states=False, 
-                                  beam_size=1, 
+                                  beam_size=beam_size, 
                                   debug=False)
 
                 if len(result) > 0:
                     result = result[0]
                     break
+                else:
+                    beam_size += 1
+
+                    if beam_size > 5:
+                        beam_size = 1
 
         return result
 
@@ -1000,6 +1008,10 @@ class Parser(nn.Module):
 
             for batch_examples in train_set.batch_iter(batch_size=self.args['batch_size'], 
                                                        shuffle=True):
+
+                if self.args['cuda']:
+                    batch_examples.cuda()
+
                 batch_examples = [e for e in batch_examples if \
                                     len(e.tgt_actions) <= self.args['decode_max_time_step']]
                 train_iter += 1
@@ -1008,7 +1020,7 @@ class Parser(nn.Module):
                 ret_val = self.score(batch_examples)
                 loss = -ret_val[0]
 
-                loss_val = torch.sum(loss).data.item()
+                loss_val = torch.sum(loss).to('cpu').data.item()
                 report_loss += loss_val
                 report_examples += len(batch_examples)
                 loss = torch.mean(loss)
@@ -1066,6 +1078,7 @@ class Parser(nn.Module):
                                                    self.vocab, 
                                                    verbose=self.args['verbose']))
         rewards = torch.from_numpy(rewards).float()
+        rewards = rewards.squeeze(-1)
 
         if self.args['cuda']:
             rewards = rewards.cuda()

src/gan/querygan_pyt.py
@@ -67,7 +67,8 @@ def DiscriminatorDataset(real_dir, fake_dir, vocab):
     combined_file.labels = y_onehot
 
     maxlen = 150 # to match CNN classifier architecture
-    sents = torch.full((combined_file.size, maxlen), combined_file.vocab.pad)
+    sents = torch.full((len(combined_file.sentences), maxlen), 
+                        combined_file.vocab.pad)
 
     for i, s in enumerate(combined_file.sentences):
         sents[i, :len(s)] = s[:150]
@@ -278,6 +279,7 @@ def run(cfg_dict):
     use_cuda = args['cuda']
 
     if not torch.cuda.is_available():
+        print("No GPU available, running on CPU.")
         use_cuda = False
 
     gargs['cuda'] = use_cuda
@@ -334,8 +336,9 @@ def run(cfg_dict):
 
     load_to_layer(netG.src_embed, glove_emb, glove_vocab)
     load_to_layer(netG.primitive_embed, glove_emb, glove_vocab, prim_vocab)
-    if gargs['cuda']: netG.cuda()
-
+    if gargs['cuda']: 
+        netG.cuda()
+        netG.optimizer.cuda()
 
     # Set up Discriminator component with given parameters
 
@@ -413,11 +416,11 @@ def run(cfg_dict):
                                            glove_vocab)
         
             for k_step in range(k_steps):
-                loss = trainer.train(dis_set)
+                loss = netD.train_single_code(dis_set)
                 print('D_step {}, K-step {} adversarial discriminator training loss: {}'.format(d_step + 1, k_step + 1, loss))
                 discriminator_losses.append(loss)
                 
-        save_progress(trainer, netG, examples, epoch, discriminator_losses, generator_losses)
+        save_progress(netD, netG, examples, epoch, discriminator_losses, generator_losses)
         
 
 def continue_training(cfg_dict, gen_chk, disc_chk, epoch=0, gen_loss=None, disc_loss=None, use_cuda=False):
@@ -491,10 +494,8 @@ def continue_training(cfg_dict, gen_chk, disc_chk, epoch=0, gen_loss=None, disc_
                         lr=netD.args['lr'], 
                         weight_decay=netD.args['wd'])
         optimizer.load_state_dict(disc_params['optimizer'])
-        trainer = Trainer(netD.args, netD, nn.MSELoss(), optimizer, device)
     else:
-        trainer = disc_chk
-        netD = trainer.model
+        netD = disc_chk
 
     rollout = Rollout(rollout_num=rollout_num, vocab=glove_vocab)
 
@@ -504,10 +505,10 @@ def continue_training(cfg_dict, gen_chk, disc_chk, epoch=0, gen_loss=None, disc_
     for ep in range(epoch, total_epochs):
         for step in range(g_steps):
             # train generator
-            samples = generate_samples(netG, seq_len, generated_num, parser)
-            hyps, examples = list(zip(*samples))
+            hyps, states, examples = generate_samples(netG, seq_len, generated_num, parser, oracle=True)
+            # hyps, examples = list(zip(*samples))
             step_begin = time.time()
-            pgloss = netG.pgtrain(hyps, examples, rollout, netD)
+            pgloss = netG.pgtrain(hyps, states, examples, rollout, netD)
             print('[Generator {}]  step elapsed {}s'.format(step, 
                                                             time.time() - step_begin))
             print('Generator adversarial loss={}'.format(pgloss))
@@ -515,20 +516,20 @@ def continue_training(cfg_dict, gen_chk, disc_chk, epoch=0, gen_loss=None, disc_
 
         for d_step in range(d_steps):
             # train discriminator
-            _ = generate_samples(netG, seq_len, generated_num, parser, writeout=True)
+            generate_samples(netG, seq_len, generated_num, parser, writeout=True)
             dis_set = DiscriminatorDataset(os.path.join(netD.args['data'], "train"), 
                                            netG.args['sample_dir'],
                                            netG.vocab)
         
             for k_step in range(k_steps):
-                loss = netD.train(dis_set)
+                loss = netD.train_single_code(dis_set)
                 print('D_step {}, K-step {} adversarial discriminator training loss: {}'.format(d_step + 1, k_step + 1, loss))
                 discriminator_losses.append(loss)
                 
-        save_progress(netG, examples, ep, discriminator_losses, generator_losses)
+        save_progress(netD, netG, examples, ep, discriminator_losses, generator_losses)
 
 
-def save_progress(trainer, netG, examples, epoch, discriminator_losses, generator_losses):
+def save_progress(netD, netG, examples, epoch, discriminator_losses, generator_losses):
     # Save Generator model state and metadata
     gen_save = os.path.join(netG.args['output_dir'], "gen_checkpoint.pth")
     gen_checkpoint = {'epoch': epoch,
@@ -543,10 +544,9 @@ def save_progress(trainer, netG, examples, epoch, discriminator_losses, generato
     # Save Discriminator model state and metadata
     disc_save = os.path.join(netG.args['output_dir'], "disc_checkpoint.pth")
     dis_checkpoint = {'epoch': epoch,
-                      'state_dict': trainer.model.state_dict(),
-                      'args': trainer.model.args,
-                      'optimizer': trainer.optimizer.state_dict(),
-                      'criterion': trainer.criterion}
+                      'state_dict': netD.state_dict(),
+                      'args': netD.args,
+                      'optimizer': netD.state_dict()}
     torch.save(dis_checkpoint, disc_save)
 
     # Save loss histories

src/gan/utilities/rollout.py
@@ -25,7 +25,7 @@ class RolloutDataset(Dataset):
     def __len__(self):
         return self.size
     def __getitem__(self, index):
-        return deepcopy(self.data[index])
+        return copy.deepcopy(self.data[index])
 
 class Rollout(object):
     def __init__(self, rollout_num, vocab):
@@ -57,11 +57,10 @@ class Rollout(object):
 
         return tree, inp
 
-    @staticmethod
-    def parse_tokens(tree):
+    def parse_tokens(self, tree):
         text = asdl_ast_to_english(tree)
-        tokens = [x for x in Tokenizer(text)]
-        result = torch.tensor(vocab.convertToIdx(tokens, '<unk>'), 
+        tokens = [x.text for x in self.tokenizer(text)]
+        result = torch.tensor(self.vocab.convertToIdx(tokens, '<unk>'), 
                               dtype=torch.long, 
                               device='cpu')
         return result
@@ -119,31 +118,23 @@ class Rollout(object):
 
             for x in tqdm(range(max_action_len), "Translating trees..."):
                 for h, hyp in enumerate(samples[x]):
-                    inputs[x][h] = parse_tokens(hyp.tree)
+                    inputs[x][h] = self.parse_tokens(hyp.tree)
 
             for j in range(max_action_len):
-                samps = torch.full((len(inputs[x]), 150), vocab.pad)
+                samps = torch.full((len(inputs[j]), 150), vocab.pad)
 
-                for idx, x in enumerate(inputs[x]):
+                for idx, x in enumerate(inputs[j]):
                     samps[idx, :len(x)] = x[:150]
 
-                samps = RolloutDataset(samps.long())
-                roll_loader = DataLoader(samps, batch_size=netD.args['batch_size'], 
-                                         shuffle=True, num_workers=4)
-                preds = []
-
-                for k, x in tqdm(enumerate(roll_loader)):
-                    if netD.args['cuda']:
-                        x = x.cuda()
-
-                    preds.append(netD.predict(x).mean().item())
+                x = samps.long().to(netD.args['device'])
+                out, _ = netD.predict(x)
 
                 if i == 0:
-                    rewards.append(sum(preds)/len(preds))
+                    rewards.append(out.numpy())
                 else:
-                    rewards[j] += sum(preds)/len(preds)
+                    rewards[j] += out.numpy()
 
-            originals = [parse_tokens(hyp.tree) for hyp in hyps]
+            originals = [self.parse_tokens(hyp.tree) for hyp in hyps]
 
             for j in tqdm(range(batch_size), desc="Rating action step {}...".format(max_action_len)):
                 samps = torch.full((len(originals), 150), vocab.pad)
@@ -151,22 +142,13 @@ class Rollout(object):
                 for idx, x in enumerate(originals):
                     samps[idx, :len(x)] = x[:150]
 
-                samps = RolloutDataset(samps.long())
-                roll_loader = DataLoader(samps, batch_size=netD.args['batch_size'], 
-                                         shuffle=True, num_workers=4)
-
-                preds = []
-
-                for k, x in tqdm(enumerate(roll_loader)):
-                    if netD.args['cuda']:
-                        x = x.cuda()
-
-                    preds.append(netD.predict(x).mean().item())
+                x = samps.long().to(netD.args['device'])
+                out, _ = netD.predict(x)
 
             if i == 0:
-                rewards.append(sum(preds)/len(preds))
+                rewards.append(out.numpy())
             else:
-                rewards[-1] += sum(preds)/len(preds)
+                rewards[-1] += out.numpy()
 
         rewards = np.array(rewards) / (1.0 * self.rollout_num)
 